e7d525c6254e082ed06a50d3602935a8c801a2c6
Ajay Kunta <ajaykunta22@gmail.com>
2026-05-06 18:33:51 -0400

    Implemented Terraform Cloud VPC module structure

---
+++ infrastructure/dev/variables.tf
@@ -0,0 +1,38 @@
+variable "aws_region" {
+  description = "AWS Region"
+  type        = string
+}
+
+variable "project_name" {
+  description = "Project Name"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment Name"
+  type        = string
+}
+
+variable "owner" {
+  description = "Project Owner"
+  type        = string
+}
+variable "vpc_cidr" {
+  description = "CIDR block for dev VPC"
+  type        = string
+}
+
+variable "public_subnet_cidrs" {
+  description = "Public subnet CIDR blocks"
+  type        = list(string)
+}
+
+variable "private_subnet_cidrs" {
+  description = "Private subnet CIDR blocks"
+  type        = list(string)
+}
+
+variable "availability_zones" {
+  description = "Availability zones"
+  type        = list(string)
+}
