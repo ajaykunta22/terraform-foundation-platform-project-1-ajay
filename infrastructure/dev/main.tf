98d5a4ecf6ef90353b079c14d30d815841733ebe
Ajay Kunta <ajaykunta22@gmail.com>
2026-05-10 21:27:36 -0400

    Add EC2 module configuration

--- infrastructure/dev/main.tf
+++ infrastructure/dev/main.tf
@@ -36,3 +36,15 @@ module "iam" {
 
   common_tags = local.common_tags
 }
+module "ec2" {
+  source = "../../modules/ec2"
+
+  project_name = var.project_name
+  environment  = var.environment
+
+  vpc_id            = module.vpc.vpc_id
+  public_subnet_id  = module.vpc.public_subnet_ids[0]
+  instance_profile_name = module.iam.ec2_instance_profile_name
+
+  common_tags = local.common_tags
+}
